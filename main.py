ff84a231fa8a04d9ab83385b2b52932858f5100b
israelavilamares <israeljoaquinavilamares@gmail.com>
2025-01-18 15:05:15 -0600

    version 1.9.9

--- main.py
+++ main.py
@@ -1,30 +1,31 @@
 from fastapi import FastAPI
-from fastapi import Form
+from fastapi import Form, Query
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI, HTTPException,status,Depends
 from fastapi.security import OAuth2PasswordRequestForm
 from fastapi.middleware.cors import CORSMiddleware
-import os
-from dotenv import load_dotenv
 from pydantic import BaseModel
-from sqlalchemy import text
+from typing import List, Annotated
+from sqlalchemy import text, Table, select, delete
 from sqlalchemy.orm import Session
+from sqlalchemy.exc import NoResultFound
+import uvicorn
 from passwords import  hash_passwords, verify_password # retorna verify un resultado booleano 
 from database import SessionLocal ,Base, engine, metadata, get_db
-from models import reflect_tables, metadata 
+from models import reflect_tables, metadata,Cita
 from jose import JWTError, jwt
 from datetime import datetime, timedelta, timezone
 import logging
 
-logging.basicConfig(level=logging.DEBUG, format="%(asctime)s - %(levelname)s - %(message)s")
-logger = logging.getLogger(__name__)
+#logging.basicConfig(level=logging.DEBUG, format="%(asctime)s - %(levelname)s - %(message)s")
+#logger = logging.getLogger(__name__)
 
 app= FastAPI()
 
 origins = [
    
     "http://localhost:3000",  # Dirección del cliente React
-    "http://127.0.0.1:3000", # Dirección donde se ejecuta React en desarrollo
+      # Dirección donde se ejecuta React en desarrollo
 ]
 
 app.add_middleware(
@@ -41,6 +42,7 @@ SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 50
 
+
 def create_access_token(data: dict, expires_delta: timedelta | None = None):
     to_encode = data.copy()
     if expires_delta:
@@ -51,11 +53,13 @@ def create_access_token(data: dict, expires_delta: timedelta | None = None):
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
 
+
 #es lo que tiene que devolver
 class Token(BaseModel):
     access_token: str
     token_type: str = "bearer"
     rol:str
+    id: int
 
 
 class OAuth2PasswordRequestFormEmail(OAuth2PasswordRequestForm):
@@ -65,6 +69,8 @@ class OAuth2PasswordRequestFormEmail(OAuth2PasswordRequestForm):
 @app.post("/login", response_model=Token)
 def login(form_data: OAuth2PasswordRequestFormEmail = Depends(), db: Session = Depends(get_db)):
     # Buscar usuario por email
+    #paciente = db.query()
+
     user = db.query(metadata.tables["usuario"]).filter_by(email=form_data.username).first()
     if not user or not verify_password(form_data.password, user.password):
         raise HTTPException(
@@ -74,7 +80,8 @@ def login(form_data: OAuth2PasswordRequestFormEmail = Depends(), db: Session = D
         )
     # Crear token de acceso
     access_token = create_access_token(data={"sub": user.email})
-    
+
+
     # Obtener el rol del usuario
     rol = user.rol
     if not rol:
@@ -82,7 +89,84 @@ def login(form_data: OAuth2PasswordRequestFormEmail = Depends(), db: Session = D
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail="error rol"
         )     
-    return {"access_token": access_token, "token_type": "bearer", "rol": rol}
+    return {"access_token": access_token, "token_type": "bearer", "rol": rol,"id":user.id}
+
+@app.delete("/citas/")
+async def Citadelete(paciente_id: int = Query(..., description="ID del paciente"), db: Session = Depends(get_db)):
+    # Fetch the cita to delete
+    tablaCita = metadata.tables["citas"]
+
+    query = select(tablaCita).where(tablaCita.c.id == paciente_id)
+    cita = db.execute(query).fetchone()
+
+    if not cita:
+        raise HTTPException(status_code=404, detail="Cita no encontrada")
+
+    try:
+        # Delete the cita
+        delete_query = delete(tablaCita).where(tablaCita.c.id == paciente_id)
+        db.execute(delete_query)
+        db.commit()
+    except Exception as e:
+        db.rollback()
+        raise HTTPException(status_code=500, detail=f"Error al eliminar la cita: {str(e)}")
+    return {"mensaje": "Cita eliminada correctamente"}
+
+
+@app.get("/citas/")
+async def obtenerCita(paciente_id: int = Query(..., description="ID del paciente"), db: Session = Depends(get_db)):
+    
+    try:
+        tablaCita = metadata.tables["citas"]
+
+       
+        query = tablaCita.select().where(tablaCita.columns.id_paciente == paciente_id)
+        result = db.execute(query).fetchall()
+       
+        # Manejo de resultados vacíos
+        if not result:
+            raise HTTPException(status_code=404, detail="No se encontraron citas del paciente.")
+
+        # Transformar las filas en objetos Pydantic
+         # Transformar los resultados en una lista compatible con Pydantic
+      # Convertir a una lista de diccionarios
+        citas = [dict(row._mapping) for row in result]
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Error interno : {str(e)}")
+
+    return citas
+
+
+class Paciente_(BaseModel):  
+    id_paciente: int
+#    fecha_naciemiento: datetime
+    sexo: str
+    direccion: str
+    is_delete: bool
+    nombre: str
+    tel: int
+
+@app.get("/pacientes/{id}/", response_model= List[Paciente_])
+async def users(id:int,db: Session = Depends(get_db))->list[Paciente_]:
+    try:
+    # Load the "pacientes" table
+        pacientes_table = metadata.tables["pacientes"]
+        # Create and execute the query
+        query = select(pacientes_table).where(pacientes_table.c.id_usuario == id)
+        result = db.execute(query).fetchall()
+       
+        # Manejo de resultados vacíos
+        if not result:
+            raise HTTPException(status_code=404, detail="No se encontraron pacientes.")
+
+        # Transformar las filas en objetos Pydantic
+         # Transformar los resultados en una lista compatible con Pydantic
+      # Convertir a una lista de diccionarios
+        pacientes = [dict(row._mapping) for row in result]
+
+        return pacientes
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Error interno : {str(e)}")
 
 
 class UsuarioCreate(BaseModel):
@@ -93,7 +177,7 @@ class UsuarioCreate(BaseModel):
 # Endpoint para registrar un usuario
 @app.post("/register")
 def registrar_usuario(usuario: UsuarioCreate, db: Session = Depends(get_db)):
-    # Verificar si el usuario ya existe por correo electrónico
+
     usuario_existente = db.query(metadata.tables["usuario"]).filter_by(email=usuario.email).first()
     if usuario_existente:
        raise HTTPException(status_code=400, detail="El email ya existe")
@@ -105,10 +189,73 @@ def registrar_usuario(usuario: UsuarioCreate, db: Session = Depends(get_db)):
         password=hashed_password,
     )
     try:
-        db.execute(nuevo_usuario)
+        result = db.execute(nuevo_usuario)
         db.commit()
+        user_id = result.inserted_primary_key[0]
     except Exception as e:
         db.rollback()
         raise HTTPException(status_code=500, detail="Error al registrar el usuario")
+    return {"mensaje": "Usuario registrado exitosamente","user_id": user_id}
 
-    return {"mensaje": "Usuario registrado exitosamente"}
+
+class Paciente(BaseModel):
+    date: str
+    sexo: str
+    id_usuario : int  
+    address : str
+    name: str
+    tel: str
+
+
+@app.post("/register/paciente/registro2")
+def registerPac(paciente: Paciente, db: Session= Depends(get_db)):
+
+    nuevo_paciente = metadata.tables["pacientes"].insert().values(
+        fecha_nacimiento=paciente.date,
+        sexo=paciente.sexo,
+        direccion=paciente.address,
+        id_usuario=paciente.id_usuario,
+        nombre=paciente.name,
+        tel=paciente.tel
+    )
+    try:
+        db.execute(nuevo_paciente)
+        db.commit()
+    except Exception as e:
+        db.rollback()
+        raise HTTPException(status_code=500, detail="Error al registrar el paciente")
+
+    return {"mensaje": "Paciente registrado exitosamente"}
+
+
+@app.post("/send/citas")
+def sendDataCitas(cita: Cita, db: Session= Depends(get_db)):
+
+    citaTabla = metadata.tables["citas"]; 
+
+    query = select(citaTabla).where((citaTabla.c.fecha == cita.fecha) & (citaTabla.c.hora == cita.hora))
+    check = db.execute(query).fetchone()
+    
+    if check:
+        raise HTTPException(status_code=400,detail=" Ya esta Registrado esa Fecha y Hora")
+
+
+    newCita = metadata.tables["citas"].insert().values(
+        id_paciente = cita.id_paciente,
+        fecha = cita.fecha,
+        hora = cita.hora,
+        motivo = cita.motivo
+    )
+    try:
+        db.execute(newCita)
+        db.commit()
+    except Exception as e:
+        db.rollback()
+        raise HTTPException(status_code=500,detail=f"Error al Registrar la Cita : {str(e)}")
+
+    return {"mensaje": "Paciente registrado exitosamente"}
+
+
+
+
+
--- models.py
+++ models.py
@@ -1,12 +1,13 @@
 # models.py
 from database import Base, engine, metadata
-
+from pydantic import BaseModel
 # Reflejar las tablas de la base de datos
 def reflect_tables():
     metadata.reflect(bind=engine)  # Esto obtiene todas las tablas de la base de datos
     for table_name, table in metadata.tables.items():
         # Crea un modelo de SQLAlchemy para cada tabla reflejada
         print(f"Reflejando tabla: {table_name}")
+        
         globals()[table_name] = type(table_name, (Base,), {
             '__tablename__': table_name,
             '__table__': table
@@ -14,3 +15,11 @@ def reflect_tables():
 
 # Llamar a la función de reflección se ven todas las tablas[pacientes,medicos,usuarios]
 reflect_tables()
+
+
+class Cita(BaseModel):
+    id_paciente: int
+    fecha: str
+    hora: str
+    motivo: str
+
