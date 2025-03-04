4c04fcd4c366299ab97caea2d7bee8371fa0f35a
israelavilamares <israeljoaquinavilamares@gmail.com>
2025-03-03 23:05:36 -0600

    version 3.0.6 consulta medico

--- routers/adm.py
+++ routers/adm.py
@@ -1,15 +1,34 @@
 from fastapi import APIRouter, Depends,HTTPException, status
 from schemas.database import get_db, metadata
 from schemas.schema import meDoctor
-#from sqlalchemy.sql import select
+from sqlalchemy import  or_
 from sqlalchemy.orm import Session
 
 router = APIRouter(tags=["administradores"])
 
+false = 0
 @router.get('/',response_model=list[meDoctor])
-def medicos(skip:int=0,limit=100,db: Session = Depends(get_db)):
+def medicos(skip:int=0,limit=100,db: Session = Depends(get_db)):#add verificacion si es true todavia existe
     doc = metadata.tables["medico"]
-    try:query = db.query(doc).offset(skip).limit(limit).all()
+    try:query = db.query(doc).filter(or_(
+        doc.c.is_delete == False,
+        doc.c.is_delete.is_(None)
+    )).offset(skip).limit(limit).all()
     except:raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Error interno al obtener los médicos")
     finally:db.close()
     return query
+
+@router.put('/')
+def editMedico(id:int,db: Session = Depends(get_db)):
+    doc = metadata.tables["medico"]
+    try:db.query(doc).filter(doc.c.id==id).update({"nombre":"Juan"})
+    except:raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Error interno al editar el médico")
+    finally:db.commit()
+    return {"mensaje":"Médico editado correctamente"}
+
+@router.post('/')
+def add():
+    
+
+
+    return {"mensaje":"Médico agregado correctamente"}
