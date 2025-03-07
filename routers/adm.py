de244616bdb08d3f9a948006eae3be239d99729f
israelavilamares <israeljoaquinavilamares@gmail.com>
2025-03-06 19:56:15 -0600

    version 3.0.7

--- routers/adm.py
+++ routers/adm.py
@@ -1,13 +1,14 @@
 from fastapi import APIRouter, Depends,HTTPException, status
 from schemas.database import get_db, metadata
 from schemas.schema import meDoctor
-from sqlalchemy import  or_
+from sqlalchemy import  or_, insert
 from sqlalchemy.orm import Session
 
 router = APIRouter(tags=["administradores"])
 
-false = 0
-@router.get('/',response_model=list[meDoctor])
+
+
+@router.get('/doctor',response_model=list[meDoctor])# here to need to return everyone doctors exist
 def medicos(skip:int=0,limit=100,db: Session = Depends(get_db)):#add verificacion si es true todavia existe
     doc = metadata.tables["medico"]
     try:query = db.query(doc).filter(or_(
@@ -15,7 +16,6 @@ def medicos(skip:int=0,limit=100,db: Session = Depends(get_db)):#add verificacio
         doc.c.is_delete.is_(None)
     )).offset(skip).limit(limit).all()
     except:raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Error interno al obtener los médicos")
-    finally:db.close()
     if not query:raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="No hay médicos registrados")
     return query
 
@@ -27,9 +27,17 @@ def editMedico(id:int,db: Session = Depends(get_db)):
     finally:db.commit()
     return {"mensaje":"Médico editado correctamente"}
 
-@router.post('/')
-def add():
-    
+@router.post('/doctor',response_model=meDoctor)
+#Newdoctor es el objeto que tiene los datos para mandar
+def medico(Newdoctor:meDoctor,db: Session = Depends(get_db)):
+    try: 
+        data = Newdoctor.model_dump()
+        stmt = insert(metadata.tables["medico"]).values(**data).returning("*")
+        result = db.execute(stmt).mappings().first()
+        db.commit()
+        return result
+    except:
+        #eliminar solo cambia el estatus
+        db.rollback()
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Error at add doctor")
 
-
-    return {"mensaje":"Médico agregado correctamente"}
